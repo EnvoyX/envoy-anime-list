3c7a0147adbe306b84c788e9e81d93c74cbf2ff4
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-01-29 17:47:17 +0700

    added rounded style to Anime List Card

--- src/components/AnimeList/index.js
+++ src/components/AnimeList/index.js
@@ -6,13 +6,13 @@ const AnimeList = ({ api }) => {
     <div className="grid md:grid-cols-5 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
       {api.data.map((anime) => {
         return (
-          <div className="shadow-xl text-center" key={anime.mal_id}>
+          <div className="shadow-xl text-center rounded-lg" key={anime.mal_id}>
             <Link href={`/${anime.mal_id}`}>
               <Image
                 src={anime.images.webp.image_url}
                 width={350}
                 height={350}
-                className="w-full md:max-h-[500px] max-h-60 object-cover"
+                className="w-full md:max-h-[500px] max-h-60 object-cover rounded-t-lg"
                 alt="anime image"
               ></Image>
               <h3 className="font-bold md:text-xl text-md p-4">
