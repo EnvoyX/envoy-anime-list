147d1e57cf6a013be04b7517bb329a40ce1c718a
Muhamad Hanif Hafizhan <muhamadhanifhafizhan@gmail.com>
2025-02-07 07:55:41 +0700

    setup prisma and sql for database

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,19 +1,20 @@
-'use client';
-import { FileSearch } from '@phosphor-icons/react';
-import Link from 'next/link';
+"use client";
+import { FileSearch } from "@phosphor-icons/react";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
+  const router = useRouter();
   return (
     <div className="min-h-screen max-w-xl mx-auto flex justify-center items-center">
       <div className="flex flex-col justify-center items-center gap-3">
         <FileSearch size={32} className="text-color-accent"></FileSearch>
         <h3 className="text-color-accent text-4xl font-bold">NOT FOUND</h3>
-        <Link
-          href={'/'}
+        <button
+          onClick={() => router.back()}
           className="text-color-primary hover:text-color-accent transition-all underline"
         >
           Back to home
-        </Link>
+        </button>
       </div>
     </div>
   );
